e74a1686e4f21d83e0a78c881265b8048f67ad1f
tracer-ics2025 <tracer@njuics.org>
2026-02-12 17:28:23 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:23 up  3:07,  1 user,  load average: 0.00, 0.01, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -11,10 +11,9 @@ void __am_timer_init()
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
   // 读取 RTC 设备（64位寄存器，分两次读取）
-  uint32_t low = inl(RTC_ADDR);      // 读低 32 位
-  uint32_t high = inl(RTC_ADDR + 4); // 读高 32 位
+  uint32_t low = *(volatile uint32_t *)0xa0000048;
+  uint32_t high = *(volatile uint32_t *)0xa000004c;
 
-  // 拼接成 64 位微秒数
   uptime->us = ((uint64_t)high << 32) | (uint64_t)low;
 }
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc)

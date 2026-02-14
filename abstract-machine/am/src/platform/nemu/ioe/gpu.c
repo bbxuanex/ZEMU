0280485ffb24186600dc4c982b918894fa7673f9
tracer-ics2025 <tracer@njuics.org>
2026-02-14 15:22:30 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:30 up 10 min,  1 user,  load average: 0.39, 0.21, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,8 +25,8 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 {
   uint32_t screen_size = inl(VGACTL_ADDR);
 
-  uint32_t h = screen_size >> 16;
-  uint32_t w = screen_size & 0xffff;
+  uint32_t w = screen_size >> 16;
+  uint32_t h = screen_size & 0xffff;
 
   *cfg = (AM_GPU_CONFIG_T){
       .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
@@ -34,6 +34,25 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->pixels && w == 0 && h == 0 && ctl->sync)
+  {
+    // 特殊情况：只同步，不绘图
+    outl(SYNC_ADDR, 1);
+    return;
+  }
+
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16; // 注意这里的高低位要和 config 保持一致
+
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = 0; j < w; j++)
+    {
+      fb[(y + i) * screen_w + (x + j)] = pixels[i * w + j];
+    }
+  }
   if (ctl->sync)
   {
     outl(SYNC_ADDR, 1);

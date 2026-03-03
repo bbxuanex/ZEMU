9538a6659d2cf5c7d7bd60c2db82132536819b0e
tracer-ics2025 <tracer@njuics.org>
2026-03-03 19:02:32 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:32 up  3:03,  0 users,  load average: 0.07, 0.09, 0.03

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -2,13 +2,22 @@
 #include <riscv/riscv.h>
 #include <klib.h>
 
-static Context* (*user_handler)(Event, Context*) = NULL;
+static Context *(*user_handler)(Event, Context *) = NULL;
 
-Context* __am_irq_handle(Context *c) {
-  if (user_handler) {
+Context *__am_irq_handle(Context *c)
+{
+  if (user_handler)
+  {
     Event ev = {0};
-    switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+    switch (c->mcause)
+    {
+    case 11:
+      ev.event = EVENT_YIELD;
+      c->mepc += 4;
+      break;
+    default:
+      ev.event = EVENT_ERROR;
+      break;
     }
 
     c = user_handler(ev, c);
@@ -20,7 +29,8 @@ Context* __am_irq_handle(Context *c) {
 
 extern void __am_asm_trap(void);
 
-bool cte_init(Context*(*handler)(Event, Context*)) {
+bool cte_init(Context *(*handler)(Event, Context *))
+{
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
@@ -30,11 +40,13 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   return true;
 }
 
-Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
+Context *kcontext(Area kstack, void (*entry)(void *), void *arg)
+{
   return NULL;
 }
 
-void yield() {
+void yield()
+{
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
@@ -42,9 +54,11 @@ void yield() {
 #endif
 }
 
-bool ienabled() {
+bool ienabled()
+{
   return false;
 }
 
-void iset(bool enable) {
+void iset(bool enable)
+{
 }

28b085d804d1fafcef342786ceed381e5a3bd1ed
tracer-ics2025 <tracer@njuics.org>
2026-03-10 21:23:46 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:46 up 44 min,  0 users,  load average: 0.01, 0.05, 0.03

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,14 +1,21 @@
 #include <common.h>
 
-static Context* do_event(Event e, Context* c) {
-  switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
+static Context *do_event(Event e, Context *c)
+{
+  switch (e.event)
+  {
+    // TODO: add more event dispatching.
+  case EVENT_YIELD:
+    printf("Boy, Done!");
+  default:
+    panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
 }
 
-void init_irq(void) {
+void init_irq(void)
+{
   Log("Initializing interrupt/exception handler...");
   cte_init(do_event);
 }

8104740126a8c12bed45fb45f5d2dc26af088208
tracer-ics2025 <tracer@njuics.org>
2026-03-20 20:12:49 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:12:49 up  2:04,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,7 +1,29 @@
 #include <common.h>
 #include "syscall.h"
 // #ifdef CONFIG_STRACE
-
+static const char *syscall_name[] = {
+    [SYS_exit] = "exit",
+    [SYS_yield] = "yield",
+    [SYS_open] = "open",
+    [SYS_read] = "read",
+    [SYS_write] = "write",
+    [SYS_kill] = "kill",
+    [SYS_getpid] = "getpid",
+    [SYS_close] = "close",
+    [SYS_lseek] = "lseek",
+    [SYS_brk] = "brk",
+    [SYS_fstat] = "fstat",
+    [SYS_time] = "time",
+    [SYS_signal] = "signal",
+    [SYS_execve] = "execve",
+    [SYS_fork] = "fork",
+    [SYS_link] = "link",
+    [SYS_unlink] = "unlink",
+    [SYS_wait] = "wait",
+    [SYS_times] = "times",
+    [SYS_gettimeofday] = "gettimeofday"};
+#define NR_syscall sizeof(syscall_name) / sizeof(syscall_name[0])
+const char *name = "Unknown Syscall_ID";
 void do_syscall(Context *c)
 {
   uintptr_t a[4];
@@ -9,7 +31,6 @@ void do_syscall(Context *c)
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
   switch (a[0])
   {
   case SYS_exit:
@@ -22,32 +43,7 @@ void do_syscall(Context *c)
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
-  // #ifdef CONFIG_STRACE
-  static const char *syscall_name[] = {
-      [SYS_exit] = "exit",
-      [SYS_yield] = "yield",
-      [SYS_open] = "open",
-      [SYS_read] = "read",
-      [SYS_write] = "write",
-      [SYS_kill] = "kill",
-      [SYS_getpid] = "getpid",
-      [SYS_close] = "close",
-      [SYS_lseek] = "lseek",
-      [SYS_brk] = "brk",
-      [SYS_fstat] = "fstat",
-      [SYS_time] = "time",
-      [SYS_signal] = "signal",
-      [SYS_execve] = "execve",
-      [SYS_fork] = "fork",
-      [SYS_link] = "link",
-      [SYS_unlink] = "unlink",
-      [SYS_wait] = "wait",
-      [SYS_times] = "times",
-      [SYS_gettimeofday] = "gettimeofday"};
-#define NR_syscall sizeof(syscall_name) / sizeof(syscall_name[0])
-  const char *name = "Unknown Syscall_ID";
   if (a[0] < NR_syscall && syscall_name[a[0]])
     name = syscall_name[a[0]];
   printf("STRACE: Syscall_ID=%s, arg1=0x%x, arg2=0x%x, arg3=0x%x, ret=0x%x. \n", name, a[1], a[2], a[3], c->GPRx);
-  // #endif
 }

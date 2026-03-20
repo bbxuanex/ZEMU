046c1a4125c44685f3175cca92bdac786260594f
tracer-ics2025 <tracer@njuics.org>
2026-03-20 19:21:01 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:21:01 up  1:12,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,40 @@
 #include <common.h>
 #include "syscall.h"
+#ifdef CONFIG_STRACE
+static const char *syscall_name[] = {
+    [SYS_exit] = "exit",
+    [SYS_yield] = "yield",
+    [SYS_open] = "open",
+    [SYS_read] = "read",
+    [SYS_write] = "write",
+    [SYS_kill] = "kill",
+    [SYS_getpid] = "getpid",
+    [SYS_close] = "close",
+    [SYS_lseek] = "lseek",
+    [SYS_brk] = "brk",
+    [SYS_fstat] = "fstat",
+    [SYS_time] = "time",
+    [SYS_signal] = "signal",
+    [SYS_execve] = "execve",
+    [SYS_fork] = "fork",
+    [SYS_link] = "link",
+    [SYS_unlink] = "unlink",
+    [SYS_wait] = "wait",
+    [SYS_times] = "times",
+    [SYS_gettimeofday] = "gettimeofday"};
+#define NR_syscall sizeof(syscall_name) / sizeof(syscall_name[a[0]])
+#endif
 void do_syscall(Context *c)
 {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0])
   {
   case SYS_exit:
-    halt(c->GPR2);
+    halt(a[1]);
     break;
   case SYS_yield:
     yield();
@@ -17,4 +43,10 @@ void do_syscall(Context *c)
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+#ifdef CONFIG_STRACE
+  const char *name = "Unknown Syscall_ID";
+  if (a[0] < NR_syscall && syscall_name[a[0]])
+    name = syscall_name[a[0]];
+  Log("STRACE: Syscall_ID=%s, arg1=0x%lx, arg2=0x%lx, arg3=0x%lx, ret=0x%lx. ", name, a[1], a[2], a[3], c->GPRx);
+#endif
 }

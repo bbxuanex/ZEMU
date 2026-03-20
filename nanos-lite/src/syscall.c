2775af8f677a2f060e5601295c4fe69e142ff3d9
tracer-ics2025 <tracer@njuics.org>
2026-03-20 22:14:32 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:32 up 44 min,  0 users,  load average: 0.11, 0.12, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include <common.h>
 #include "syscall.h"
-// #ifdef CONFIG_STRACE
+#ifdef CONFIG_STRACE
 static const char *syscall_name[] = {
     [SYS_exit] = "exit",
     [SYS_yield] = "yield",
@@ -24,6 +24,7 @@ static const char *syscall_name[] = {
     [SYS_gettimeofday] = "gettimeofday"};
 #define NR_syscall sizeof(syscall_name) / sizeof(syscall_name[0])
 const char *name = "Unknown Syscall_ID";
+#endif
 void do_syscall(Context *c)
 {
   uintptr_t a[4];
@@ -63,7 +64,9 @@ void do_syscall(Context *c)
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+#ifdef CONFIG_STRACE
   if (a[0] < NR_syscall && syscall_name[a[0]])
     name = syscall_name[a[0]];
   printf("STRACE: Syscall_ID=%s, arg1=0x%x, arg2=0x%x, arg3=0x%x, ret=0x%x. \n", name, a[1], a[2], a[3], c->GPRx);
+#endif
 }

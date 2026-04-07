471d101f9d893e4d917417113916358abeb76ca8
tracer-ics2025 <tracer@njuics.org>
2026-04-07 20:11:14 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:14 up  1:15,  0 users,  load average: 0.00, 0.01, 0.00

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,62 +3,100 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+static unsigned long long time_libst;
 
-uint32_t NDL_GetTicks() {
+static inline uint32_t convert_timeval_to_ms(struct timeval *tv)
+{
+  return (uint32_t)(unsigned long long)tv->tv_sec * 1000 + (unsigned long long)tv->tv_usec / 1000;
+}
+
+uint32_t NDL_GetTicks()
+{
+  unsigned long long time;
+  unsigned long long time_for_media;
+  struct timeval unproc_time;
+
+  gettimeofday(&unproc_time, NULL);
+
+  time = convert_timeval_to_ms(&unproc_time);
+  time_for_media = time - time_libst;
+
+  return (uint32_t)time_for_media;
+}
+
+int NDL_PollEvent(char *buf, int len)
+{
   return 0;
 }
 
-int NDL_PollEvent(char *buf, int len) {
-  return 0;
-}
-
-void NDL_OpenCanvas(int *w, int *h) {
-  if (getenv("NWM_APP")) {
+void NDL_OpenCanvas(int *w, int *h)
+{
+  if (getenv("NWM_APP"))
+  {
     int fbctl = 4;
     fbdev = 5;
-    screen_w = *w; screen_h = *h;
+    screen_w = *w;
+    screen_h = *h;
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
     write(fbctl, buf, len);
-    while (1) {
+    while (1)
+    {
       // 3 = evtdev
       int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
+      if (nread <= 0)
+        continue;
       buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
+      if (strcmp(buf, "mmap ok") == 0)
+        break;
     }
     close(fbctl);
   }
 }
 
-void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
+{
 }
 
-void NDL_OpenAudio(int freq, int channels, int samples) {
+void NDL_OpenAudio(int freq, int channels, int samples)
+{
 }
 
-void NDL_CloseAudio() {
+void NDL_CloseAudio()
+{
 }
 
-int NDL_PlayAudio(void *buf, int len) {
+int NDL_PlayAudio(void *buf, int len)
+{
   return 0;
 }
 
-int NDL_QueryAudio() {
+int NDL_QueryAudio()
+{
   return 0;
 }
 
-int NDL_Init(uint32_t flags) {
-  if (getenv("NWM_APP")) {
+int NDL_Init(uint32_t flags)
+{
+  if (getenv("NWM_APP"))
+  {
     evtdev = 3;
   }
+  static struct timeval libst_time;
+  gettimeofday(&libst_time, NULL);
+
+  time_libst = convert_timeval_to_ms(&libst_time);
+
   return 0;
 }
 
-void NDL_Quit() {
+void NDL_Quit()
+{
+  time_libst = 0;
 }
--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,26 +1,25 @@
-#include <sys/time.h>
 #include <stdio.h>
+#include <NDL.h>
 int main(void)
 {
-    struct timeval tv;
-    unsigned long long last = 0;
+    NDL_Init(0);
+    uint32_t last = NDL_GetTicks();
 
     while (1)
     {
-        gettimeofday(&tv, NULL);
-
-        unsigned long long now = (unsigned long long)tv.tv_usec + (unsigned long long)tv.tv_sec * (unsigned long long)1000000;
+        uint32_t now = NDL_GetTicks();
 
         if (last == 0)
         {
             last = now;
             continue;
         }
-        if (now - last >= 500000)
+        if (now - last >= 500)
         {
             printf("time has just passed 0.5s!\n");
             last = now;
         }
     }
+    NDL_Quit();
     return 0;
 }

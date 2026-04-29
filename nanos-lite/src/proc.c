f8568b4d01249ff700baf1cc0b3d49fdd0d95fd1
tracer-ics2025 <tracer@njuics.org>
2026-04-29 21:20:24 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:24 up  2:02,  0 users,  load average: 0.27, 0.27, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc()
   Log("Initializing processes...");
 
   // load program tere
-  naive_uload(NULL, "/bin/nslider");
+  naive_uload(NULL, "/bin/bmp-test");
 }
 
 Context *schedule(Context *prev)

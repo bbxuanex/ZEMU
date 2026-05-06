fea38d3219bdc763fbb05f40aa450e6517564302
tracer-ics2025 <tracer@njuics.org>
2026-05-06 19:24:02 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:02 up 18 min,  0 users,  load average: 0.00, 0.02, 0.06

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,29 +1,83 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <sdl-event.h>
 
 #define keyname(k) #k,
+#define numkey (sizeof(keyname) / sizeof(keyname[0]))
 
 static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+    "NONE",
+    _KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
+int SDL_PushEvent(SDL_Event *ev)
+{
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
+int SDL_PollEvent(SDL_Event *ev)
+{
   return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
+int SDL_WaitEvent(SDL_Event *event)
+{
+  while (1)
+  {
+    int ret;
+    char buf[64];
+    char keytpstr[4];
+    char keynmstr[32];
+
+    ret = NDL_PollEvent(buf, sizeof(buf));
+    if (!ret)
+      continue;
+
+    int n = sscanf(buf, "%3s %31s", keytpstr, keynmstr);
+    if (n != 2)
+      continue;
+
+    int sdl_keytyp = -1;
+    int sdl_keynm = -1;
+
+    if (strcmp(keytpstr, "ku") == 0)
+      sdl_keytype = SDL_KEYUP;
+    if (strcmp(keytpstr, "kd") == 0)
+      SDL_keytype = SDL_KEYDOWN;
+
+    if (sdl_keytp < 0)
+      continue;
+
+    for (int i = 0; i < numkey; ++i)
+    {
+      if (strcmp(keynmstr, keyname[i]) == 0)
+      {
+        sdl_keynm = i;
+        break;
+      }
+    }
+
+    if (sdl_keynm < 0)
+      continue;
+
+    if (event == NULL)
+      return 1;
+
+    event->type = sdl_keytype;
+    event->key.type = sdl_keytype;
+    event->key.keysym.sym = sdl_keynm;
+
+    return 1;
+  }
+
   return 1;
 }
 
-int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
+{
   return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
+uint8_t *SDL_GetKeyState(int *numkeys)
+{
   return NULL;
 }
